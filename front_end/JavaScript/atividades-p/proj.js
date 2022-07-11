7e87c10916cdd8bd713a2f9d897b47495b2b670f
ViniciusF <vinxz0014@gmail.com>
2022-07-11 11:22:42 -0300

    Atulizacao de aula JS

---
+++ front_end/JavaScript/atividades-p/proj.js
@@ -0,0 +1,14 @@
+
+
+const N=19;
+let i;
+    
+for ( let i = 1; i <= N; i++) {
+  if (i % 2 === 0) {
+  let squaredNumber = Math.pow(i,2);
+      console.log(i+'^2 =',squaredNumber);
+      
+    }
+
+  }
+
---
+++ front_end/JavaScript/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade,mostraIdade,mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Viny",20));
+console.log(mostraHobby("Vinicius","Video-Game"));
+console.log(mostraCidade("Viyy","Goiania"));

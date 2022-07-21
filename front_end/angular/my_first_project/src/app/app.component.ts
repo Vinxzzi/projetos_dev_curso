7744f0611b564962ae4b68aafda999cd8cd7181a
Vinxzzi <vinxz0014@gmail.com>
2022-07-21 11:30:45 -0300

    Aluna: Projeto de lista Angular

--- front_end/angular/my_first_project/src/app/app.component.ts
+++ front_end/angular/my_first_project/src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Meu Primeiro Projeto';
+export class AppComponent implements OnInit{
+  count =0;
+
+
+  pessoas=[
+
+  {
+    nome:"Vinicius",
+    sobrenome:"Ferrreira"
+
+  },
+
+  {
+    nome:"Adriana",
+    sobrenome:"Ferrreira"
+  },
+  {
+    nome:"Haroldo",
+    sobrenome:"Vieira"
+  }
+  ]
+
+  text=''
+
+  ngOnInit(): void {
+      let intervalo = setInterval(()=>{
+        
+        this.count++;
+        if(this.count===10){
+          clearInterval(intervalo);
+        }
+      },1000)
+  }
+
+
 }
